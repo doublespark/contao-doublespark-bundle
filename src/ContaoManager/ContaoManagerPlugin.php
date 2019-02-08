4ea786bf4733a9a917d58c4ab615f842fa2ac569
Jamie Devine <jamie.devine@doublespark.co.uk>
2019-02-08 12:36:43 +0000

    Make compatible with contao manager

---
+++ src/ContaoManager/ContaoManagerPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Doublespark\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Doublespark\DoublesparkBundle;
+
+class ContaoManagerPlugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(DoublesparkBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['doublespark']),
+        ];
+    }
+}
