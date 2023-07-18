c7005ba7d71ca50e3cff9cfc01418484b4dc0e9a
Jamie Devine <jamie.devine@doublespark.co.uk>
2023-07-18 12:19:28 +0100

    Add label

--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -7,3 +7,5 @@ use Doublespark\ContaoDoublesparkBundle\Elements\ContentGridEndElementController
 $GLOBALS['TL_LANG']['CTE'][BannerImageElementController::TYPE]      = ['Banner Image', 'A banner image with optional text overlay'];
 $GLOBALS['TL_LANG']['CTE'][ContentGridStartElementController::TYPE] = ['Content grid start', 'Open a content grid'];
 $GLOBALS['TL_LANG']['CTE'][ContentGridEndElementController::TYPE]   = ['Content grid end', 'Close a content grid'];
+
+$GLOBALS['TL_LANG']['CTE']['layout'] = 'Layout elements';
